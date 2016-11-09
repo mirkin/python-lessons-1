c2c2aed0dc20789715d2da2836d718885316ef6c
Alex <alex@matica.com>
2016-11-09 20:18:41 +0000

    hangman 2 progress

--- hangman_2.py
+++ hangman_2.py
@@ -67,48 +67,58 @@ def map_char_from_word1_to_word2(word1,word2,search_char,replace_char):
         word2=replace_letter_in_word(word2,replace_char,loc)
     return word2
 
-print(title) # Print our awesome title
-lives=10 # Reset lives to 10
-word=random.choice(word_list) # Picks a word at random from our word_list
-word_lower=word.lower() # For checking let's use lowercase to avoid confusion
-# Build up the guess blanks depending on length of the word
-guess=blank_character*len(word)
-# Show spaces in the guess
-guess=map_char_from_word1_to_word2(word_lower,guess,' ',word_seperator)
-# Show any dashes in guess
-guess=map_char_from_word1_to_word2(word_lower,guess,'-','-')
-while True: # Infinte loop we will break if we win or lose
-    print('')
-    print(guess)
-    print("You have {} guesses left".format(lives))
-    # Continually request a letter change it to lower case just in case it's
-    # upper. Only move on if it's a valid guess
-    while True:
-        letter=input("Please type a letter: ").lower()
-        if validate_letter(letter):
-            break
-    # Find will return -1 if the letter isn't there so reduce the lives and
-    # print a failure message
-    if word_lower.find(letter)==-1:
-        lives-=1
-        print("Bad luck the word doesn't contain '"+letter+"'")
-    else:
-        # The letter must be there so get a list of locations of that letter
-        locations=search_for_letter_in_word(word_lower,letter)
-        # Loop through the list of locations and copy the letter at that
-        # location in the original word to that location in the guess and print
-        # a success message
-        for loc in locations:
-            guess=replace_letter_in_word(guess,word[loc],loc)
-        print("Good guess the word did contain '"+letter+"'")
-    # If live have run out break the loop and print failure message
-    if lives<=0:
-        print("Too bad you ran out of guesses, the word was "+word)
-        break
-    # If there are no blanks left break the loop and print success message
-    if guess.find(blank_character)==-1:
+def play_game():
+    """ Play a round of hangman
+    """
+    print(title) # Print our awesome title
+    lives=10 # Reset lives to 10
+    word=random.choice(word_list) # Picks a word at random from our word_list
+    # For checking let's use lowercase to avoid confusion
+    word_lower=word.lower()
+    # Build up the guess blanks depending on length of the word
+    guess=blank_character*len(word)
+    # Show spaces in the guess
+    guess=map_char_from_word1_to_word2(word_lower,guess,' ',word_seperator)
+    # Show any dashes in guess
+    guess=map_char_from_word1_to_word2(word_lower,guess,'-','-')
+    while True: # Infinte loop we will break if we win or lose
+        print('')
         print(guess)
-        print("Well done, you guessed the word")
+        print("You have {} guesses left".format(lives))
+        # Continually request a letter change it to lower case just in case it's
+        # upper. Only move on if it's a valid guess
+        while True:
+            letter=input("Please type a letter: ").lower()
+            if validate_letter(letter):
+                break
+        # Find will return -1 if the letter isn't there so reduce the lives and
+        # print a failure message
+        if word_lower.find(letter)==-1:
+            lives-=1
+            print("Bad luck the word doesn't contain '"+letter+"'")
+        else:
+            # The letter must be there so get a list of locations of that letter
+            locations=search_for_letter_in_word(word_lower,letter)
+            # Loop through the list of locations and copy the letter at that
+            # location in the original word to that location in the guess and
+            # print a success message
+            for loc in locations:
+                guess=replace_letter_in_word(guess,word[loc],loc)
+            print("Good guess the word did contain '"+letter+"'")
+        # If live have run out break the loop and print failure message
+        if lives<=0:
+            print("Too bad you ran out of guesses, the word was "+word)
+            break
+        # If there are no blanks left break the loop and print success message
+        if guess.find(blank_character)==-1:
+            print(guess)
+            print("Well done, you guessed the word")
+            break
+    print('')
+    print('Thanks for playing.')
+
+while True:
+    play_game()
+    play_again=input('Play again? (y/n) ')
+    if play_again=='n':
         break
-print('')
-print('Thanks for playing.')
